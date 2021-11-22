4f2e6151c6165c54ad99077ca579ca8c197a2609
karakurtserdar19@gmail.com <karakurtserdar19@gmail.com>
2021-11-22 19:51:39 +0300

    Add project files.

---
+++ kurs_22112021_40_Example2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs_22112021_40_Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
