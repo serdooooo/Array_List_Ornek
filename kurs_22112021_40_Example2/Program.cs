d414605d87eea5c54f3b865ac338d26d1dcdbdc0
karakurtserdar19@gmail.com <karakurtserdar19@gmail.com>
2021-11-22 20:01:46 +0300

    new

--- kurs_22112021_40_Example2/Program.cs
+++ kurs_22112021_40_Example2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Threading;
 
 namespace kurs_22112021_40_Example2
 {
@@ -10,6 +12,47 @@ namespace kurs_22112021_40_Example2
     {
         static void Main(string[] args)
         {
+            //ArrayList'te tip yok
+            ArrayList listem = new ArrayList();
+            listem.Add("Faik");
+            listem.Add("Turan");
+            listem.Add("123456789");
+            listem.Add('E');
+            listem.Add(20);
+
+            listem.Add("Esra");
+            listem.Add("Turan");
+            listem.Add("123456789");
+            listem.Add('K');
+            listem.Add(20);
+
+            listem.Add("Arif");
+            listem.Add("Turan");
+            listem.Add("123456789");
+            listem.Add('K');
+            listem.Add(20);
+
+            Console.WriteLine("Array List oluşturuldu.");
+            Console.WriteLine("Yazdırılıyor...");
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            foreach (var item in listem)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Array List kapasitesi: "+listem.Capacity);
+
+            listem.Remove("Faik");
+            int[] l = new int[5];
+            foreach (var item in listem.ToArray())
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
